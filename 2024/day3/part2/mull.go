ae51a033dae18cf7687e2aa7d6601baac0e4bff0
Zederbear <hooverzed@gmail.com>
2024-12-03 13:40:50 -0700

    removed commented code

--- 2024/day3/part2/mull.go
+++ 2024/day3/part2/mull.go
@@ -67,40 +67,4 @@ func main() {
     
     fmt.Println("Total: ")
     fmt.Println(total)
-
-    /* 
-    fmt.Println("Matches found: ")
-
-    for _, match := range matches {
-        fmt.Println(match)
-    }
-
-    var result [][]int
-
-    for _, match := range matches {
-        if len(match) == 3 {
-            num1, err1 := strconv.Atoi(match[1])
-            num2, err2 := strconv.Atoi(match[2])
-
-            if err1 != nil || err2 != nil {
-                log.Fatal(err1, err2)
-            }
-
-            result = append(result, []int{num1, num2})
-        }
-    }
-    
-    fmt.Println("Extracted numbers:")
-    for _, pair := range result {
-        fmt.Println(pair)
-    }
-
-    var total int
-    for _, pair := range result {
-        total += pair[0] * pair[1]
-    }
-    
-    fmt.Println("Total: ")
-    fmt.Println(total)
-    */
 }
